9182146d55cc180b26e378b74c45909bf4deb26e
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-02-02 19:55:42 +1100

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    thingTodos(e.target.querySelector('#new-task-description').value)
+    
+  })
 });
+
+function thingTodos(todo){
+ let li = document.createElement('li')
+ let btn = document.createElement('button')
+ btn.addEventListener('click',deleteThingToDo)
+ btn.textContent = "x"
+ li.textContent = `${todo} `;
+ li.appendChild(btn);
+ document.querySelector('#tasks').appendChild(li);
+ 
+}
+function deleteThingToDo(e){
+  e.target.parentNode.remove()
+}
